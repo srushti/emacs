24f1c1a25cb17a20fc8e8c4a6cbcfb276175904f
Srushti Ambekallu <srushti@ambekallu.com>
2010-02-07 19:42:06 +0530

    got rid of duplicate A-d definition

--- key-bindings.el
+++ key-bindings.el
@@ -1,7 +1,6 @@
 (global-set-key "\177" 'c-hungry-delete-backwards)
 (global-set-key "\C-d" 'c-hungry-delete-forward)
 
-(global-set-key (kbd "A-d") "\C-a\C- \C-n\M-w\C-y")
 (global-set-key (kbd "A-D") "\M-w\C-y")
 
 (global-set-key "\C-cc" 'mode-compile)
