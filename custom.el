a1790f65015f8eb65d1cf017dca247a48814ffe5
Niranjan Paranjape <niranjan@c42.in>
2010-02-09 19:39:55 +0530

    merging with irfn

--- custom.el
+++ custom.el
@@ -1,45 +1,27 @@
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1)) Need this on osx
-(setq inhibit-splash-screen t)
-
-(setq make-backup-files nil)
 (custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-activation-selects-ecb-frame-if-already-active t)
+ '(ecb-compilation-buffer-names (quote (("*vc*") ("*vc-diff*") ("*anything*") ("*Apropos*") ("*eshell*") ("*Occur*") ("*shell*" . t) ("\\*[cC]ompilation.*\\*" . t) ("\\*i?grep.*\\*" . t) ("*Help*") ("*Completions*") ("*Backtrace*") ("*Compile-log*") ("*Messages*") ("*magit: " . t))))
+ '(ecb-compile-window-height 6)
+ '(ecb-compile-window-temporally-enlarge (quote both))
+ '(ecb-compile-window-width (quote frame))
+ '(ecb-layout-name "left7")
+ '(ecb-maximize-ecb-window-after-selection t)
+ '(ecb-mode-line-display-window-number nil)
+ '(ecb-options-version "2.40")
+ '(ecb-tip-of-the-day nil)
+ '(ecb-tree-indent 2)
+ '(ecb-vc-enable-support t)
+ '(ecb-windows-width 0.2)
+ '(linum-format "%4d ")
  '(show-paren-mode t)
  '(transient-mark-mode t))
-(c-mode)
-
-(c-toggle-hungry-state)
-
-(global-set-key "\177" 'c-hungry-delete-backwards)
-(global-set-key "\C-d" 'c-hungry-delete-forward)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq default-tab-width 2)
-
-(setq rinari-tags-file-name "TAGS")
-(setq my-global-projects-dirs (quote (
-"~/Work/ruby/ror"
-"~/Work/snippets"
-)))
-
-(require 'maxframe)
-(add-hook 'window-setup-hook 'maximize-frame t)
-
-(autoload 'mode-compile "mode-compile"
-   "Command to compile current buffer file based on the major mode" t)
-(global-set-key "\C-cc" 'mode-compile)
- 
-(autoload 'mode-compile-kill "mode-compile"
- "Command to kill a compilation launched by `mode-compile'" t)
-(global-set-key "\C-ck" 'mode-compile-kill)
-
-(defun ruby-outline-mode ()
-  (interactive)
-  (setq outline-regexp " *\\(def\\|if\\|when\\|do\\)"))
-
-(setq textmate-gf-exclude 
-  "/\\.|vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.pyc|\\.elc|\\.jar")
-(setq auto-mode-alist (cons '("Rakefile$" . ruby-mode) auto-mode-alist))
-
-(global-set-key (kbd "A-d") "\C-a\C- \C-n\M-w\C-y")
-(global-set-key (kbd "A-D") "\M-w\C-y")
-(setq highlight-current-line-globally t)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
---
+++ key-bindings.el
@@ -0,0 +1,19 @@
+(global-set-key "\177" 'c-hungry-delete-backwards)
+(global-set-key "\C-d" 'c-hungry-delete-forward)
+
+(global-set-key (kbd "A-d") "\C-a\C- \C-n\M-w\C-y")
+(global-set-key (kbd "A-D") "\M-w\C-y")
+
+(global-set-key "\C-cc" 'mode-compile)
+(global-set-key "\C-ck" 'mode-compile-kill)
+
+(global-set-key (kbd "\C-cd") 'duplicate-line)
+(global-set-key (kbd "ESC M-d") 'ecb-toggle-ecb-windows)
+
+(define-key ac-completing-map "\M-n" 'ac-next)
+(define-key ac-completing-map "\M-p" 'ac-previous)
+(define-key ac-mode-map (kbd "\M-/") 'auto-complete) 
+(global-set-key "\M-/" 'auto-complete)
+(global-set-key "\M-?" 'dabbrev-expand)
+(global-set-key (kbd "\C-co") 'open-buffer-path)
+(define-key anything-map "\C-cz" 'anything-execute-persistent-action)
